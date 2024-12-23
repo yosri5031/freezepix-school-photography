75f0a9eba5e043ba72283bea3f7d7a218eb3e67f
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2024-12-23 16:57:52 +0100

    Update helcimService.js

--- src/helcimService.js
+++ src/helcimService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_TOKEN = 'aM2T3NEpnksEOKIC#ajd%!-IE.TRXEqUIi_Ct8P.K18z1L%aV3zTl*R4PHoDco%y';
+const API_TOKEN = 'aDjyTb2SDE1wgdf!xIj5OWynKkW4%mHRurFV.UscZMSO#6muhq6OzDzM7BCdCSzT';
 const HELCIM_API_URL = 'https://api.helcim.com/v2/helcim-pay/initialize';
 
 export const initializeHelcimPayCheckout = async ({
@@ -11,7 +11,7 @@ export const initializeHelcimPayCheckout = async ({
     const response = await axios.post('https://freezepix-database-server-c95d4dd2046d.herokuapp.com/api/initialize-payment', {
       selectedCountry,
       total
-        });
+    });
 
     return {
       checkoutToken: response.data.checkoutToken,
